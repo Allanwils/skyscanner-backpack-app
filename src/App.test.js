9b6e524dcffad2d6867b920c06f16614b2a188de
ALLAN MASIGA <zionalehebrew@gmail.com>
2024-08-23 18:41:15 +0300

    upload the application

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
 describe('App', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
+    const root = createRoot(div);
 
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    root.render(<App />);
+    root.unmount();
   });
 });
+
